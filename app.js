575569fedde8114d9ad464f8193fea8e081f269e
EvaMeza10 <mezaeva10@gmail.com>
2024-07-22 14:54:43 -0400

    seccion 4.1

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-let nombre = "Eva";
-console.log(nombre);
+// Crear un algoritmo, que basado en la nota
+// Imprima si el alumn o aprueba la materia
+// Aprueba 60
+// Si el alumno tinen nota menor a 50
+// Imprimir un mensaje que diga:
+// Por favor estudie mas
+
+let nota= 40;
+if (nota >= 60){
+    console.log("El alumno aprueba la clase");
+
+}else if (nota < 50){
+    console.log("Por favor estudie mas");
+
+}else {
+    console.log9m("El alumno NO aprueba la clase");
+}
+console.log("Fin del programa");
